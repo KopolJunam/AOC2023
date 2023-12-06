d27f6ba5ae826733f8c770c0774d0269a514e5a7
Thomas <thomas.maurer@ierax.ch>
2023-12-06 06:48:35 +0100

    Lösung Tag 2 - beide Teile

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,32 +1,53 @@
 import java.io.File
 
-val maxValue = mapOf(
-    "red" to 12,
-    "green" to 13,
-    "blue" to 14
-)
-
 fun main() {
+    solvePart1()
+    solvePart2()
+ }
+
+fun solvePart1() {
     println(
         File("c:/temp/aoc2.txt").inputStream().bufferedReader().lines().
         map { line -> line.split(":", ";", ",") }.
         mapToInt {
             lineTokens ->
+            val maxValue = mapOf("red" to 12, "green" to 13,"blue" to 14)
             var gameNo = 0
             if(lineTokens.stream().
                 peek {
                     token -> gameNo = if(token.startsWith("Game"))
-                                        Integer.parseInt(token.split(" ")[1])
-                                      else
-                                        gameNo
+                    Integer.parseInt(token.split(" ")[1])
+                else
+                    gameNo
                 }.
                 filter { !it.startsWith("Game") }.
-                noneMatch { val entry = it.trim().split(" ")
-                            Integer.parseInt(entry[0]) > maxValue[entry[1]]!! }
+                noneMatch {
+                    val entry = it.trim().split(" ")
+                    Integer.parseInt(entry[0]) > maxValue[entry[1]]!!
+                }
             )
                 gameNo
             else
                 0
+        }.
+        sum()
+    )
+}
+
+fun solvePart2() {
+    println(
+        File("c:/temp/aoc2.txt").inputStream().bufferedReader().lines().
+        map { line -> line.split(":", ";", ",") }.
+        mapToInt {
+            lineTokens ->
+            val maximum = mutableMapOf( "red" to 0, "green" to 0, "blue" to 0)
+            lineTokens.stream().
+            skip(1).
+            forEach {
+                val entry = it.trim().split(" ")
+                maximum.put(entry[1], Math.max(maximum[entry[1]]!!, Integer.parseInt(entry[0])))
+            }
+            maximum["red"]!! * maximum["green"]!! * maximum["blue"]!!
         }.
         sum()
     )
